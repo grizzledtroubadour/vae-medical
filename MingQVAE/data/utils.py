af850b051392ca9085abe869f7ca8bfbb3d2f4ed
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 02:21:20 +0000

    Adding tarfile member sanitization to extractall()

--- MingQVAE/data/utils.py
+++ MingQVAE/data/utils.py
@@ -13,10 +13,48 @@ def unpack(path, datadir=False):
     # extract all to datadir
     if path.endswith("tar.gz"):
         with tarfile.open(path, "r:gz") as tar:
-            tar.extractall(path=datadir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=datadir)
     elif path.endswith("tar"):
         with tarfile.open(path, "r:") as tar:
-            tar.extractall(path=datadir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=datadir)
     elif path.endswith("zip"):
         with zipfile.ZipFile(path, "r") as f:
             f.extractall(path=datadir)
